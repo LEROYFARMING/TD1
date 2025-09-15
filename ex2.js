2263025e516e28c88f12574b64b6bce178039ece
LEROYFARMING <leroy.farming@gmail.com>
2025-09-15 15:07:37 +0200

    EX2 "Aire Rectangle" Finish

---
+++ ex2.js
@@ -0,0 +1,6 @@
+var longueur = 25;
+var largeur = 11;
+
+var aireRectangle = longueur * largeur;
+
+console.log("L'aire du rectangle de largeur " + largeur + " et de longueur " + longueur + " est " + aireRectangle);
