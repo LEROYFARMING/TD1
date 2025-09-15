c10880d7f2a015be864d305c9646bc0d5bcfeb35
LEROYFARMING <leroy.farming@gmail.com>
2025-09-15 14:56:37 +0200

    commit pour test

--- ex1.js
+++ ex1.js
@@ -2,3 +2,5 @@ var temperatureCelsius = 10;
 var temperatureFahrenheit = temperatureCelsius * 9/5 + 32;
 
 print("Température en degrés Celsius : " + temperatureCelsius + " Température en degrés Fahrenheit : " + temperatureFahrenheit);
+
+/*test*/
