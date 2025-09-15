4bff5d92755fa91d4df235df708fe5383dda4de2
LEROYFARMING <leroy.farming@gmail.com>
2025-09-15 15:44:37 +0200

    EX7 "Condition" Finish

---
+++ ex7.js
@@ -0,0 +1,10 @@
+const minimumCommandeLivraisonGratuite = 2;
+let totalCommande = 5;  /*totalCommande a changé pour voir le changement*/
+
+if(totalCommande >= minimumCommandeLivraisonGratuite) {
+    console.log("Livraison gratuite !")
+}
+else {
+    console.log("Frais de livraison : 30 euros.");
+}
+
